64f31c9ed4ea25c0249b62039b44adb955ce0437
kaidenhwang23 <kaidenhwang@gmail.com>
2020-11-18 19:40:50 -0800

    Update sketch.js

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -6,41 +6,52 @@ function setup() {
   createCanvas(800, 400);
 
   // construct the ball using the 'new' keyword
-  b1 = new Ball(400,200,random(0,255), random(0,255), random(0,255))
-  b2 = new Ball(400,200,random(0,255))
+  b1 = new Ball(400, 200, 150, 150)
+  b2 = new Ball(0, 200, 245, 50)
 }
 
 
-function draw(){
-	background(220){
- //draw the ball called b (go look in the Ball class for the drawBall function)
- //move the ball called b (go look in the Ball class for the moveBall function)
-b1.drawBall()
-b1.moveBall()
-b2.drawBall()
-b2.moveBall()
-}
+function draw() {
+  background(220); {
+    //draw the ball called b (go look in the Ball class for the drawBall function)
+    //move the ball called b (go look in the Ball class for the moveBall function)
+    b1.drawBall()
+    b1.moveBall()
+    b2.drawBall()
+    b2.moveBall()
+  }
 }
 
 
 //ball class from which to create new balls with similar properties.
 class Ball {
-  constructor(x,y,color){
+  constructor(x, y, color, size) {
     this.x = x
     this.y = y
     this.color = color
+    this.size = size
   }
 
-  drawBall(){
+  drawBall() {
     stroke(255)
     strokeWeight(5)
     fill(this.color)
-    ellipse(this.x,this.y,150,150)
+    ellipse(this.x, this.y, this.size)
   }
 
-  moveBall(){
-    this.x = mouseX + random(-100,100)
-    this.y = mouseY + random(-10,10)
+  moveBall() {
+    if (frameCount % 50 == 0) {
+      if (this.x < 800) {
+        this.x += 50
+        this.size += 25
+        this.color -= 20
+      } else {
+        this.x = -50
+        this.size = 50
+        this.color = 255
+      }
+
+    }
   }
 
   // here is where you'll put the instructions for making balls
