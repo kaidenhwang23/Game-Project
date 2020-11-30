f00acc070616e7f8c66b8eb8e72cf32177618c30
kaidenhwang23 <kaidenhwang@gmail.com>
2020-11-30 11:11:23 -0800

    sounds

--- 4-sound/sketch.js
+++ 4-sound/sketch.js
@@ -1,119 +1,62 @@
 //create an empty array called balls
-let balls = [];
+
 
 //create a variable to hold your avatar
-let me;
 
-let mySound;
+let mySound2;
+let mySound3;
+let mySound4;
+let mySound5;
+let birds = [];
+
+
 
 function preload() {
   soundFormats('mp3', 'ogg', 'wav');
-  mySound = loadSound('boing1.mp3');
   mySound2 = loadSound('birdnoise.wav')
   mySound3 = loadSound('leafnoise.wav')
   mySound4 = loadSound('windchimes.wav')
-  mySound5 = loadSound('windy')
+  mySound5 = loadSound('windy.wav')
 }
 
 function setup() {
   createCanvas(500, 400);
-
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
+
 
 }
 
 function draw(){
 	background(220);
-
-  me.drawMe();
-  me.moveMe();
-
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
-    }
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
-	  }
-
-}
-
-//avatar class
-class Avatar {
-
-	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
-		    this.x = x;
-    		this.y = y;
-        this.speed = speed;
-	}
-
-	drawMe(){  // draw the running person
-    		stroke("green");
-        strokeWeight(3);
-    		fill("blue");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
-	}
-
-	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
-    }
-
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
-    }
-	}
-
-  die(){
-
+  for(i = 0; i< birds.length; i++){
+    bird.drawBird
   }
 
 }
 
 
-//ball class from which to create new balls with similar properties.
-class Ball {
+function keyPressed(){
+  if (keyCode == 49){
+    mySound2.play()
+    bird = new Birb(random(0,width), random(30,60))
+    birds.push(bird)
 
-	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
-		this.x = x;
-		this.y = y;
-  	this.speed = speed;
-	}
 
-	// draw a ball on the screen at x,y
-	drawBall(){
-		stroke(0);
-  	strokeWeight(1);
-		fill("red");
-		ellipse(this.x,this.y,10,10);
-	}
+  }
+}
 
-	//update the location of the ball, so it moves across the screen
-	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
-	}
+class Birb {
+  constructor(x, y){
+    this.x = x
+    this.y = y
 
-	//if the ball hits the paddle, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-            mySound.setVolume(0.1);
-            mySound.play();
-    		}
-  	}
+  drawBird(){
+    fill("yellow")
+    stroke("black")
+    rect(this.x,this.y,20,20,5)
+    }
 
+
+
+  }
 }
